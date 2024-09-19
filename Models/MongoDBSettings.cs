fb6531f96721a4a4f68306495d7f1b941bf93826
ShehaniDehipola <shehanidehipola@gmail.com>
2024-09-19 07:17:07 +0530

    inital commit : web service (MongoDB connection Added)

---
+++ Models/MongoDBSettings.cs
@@ -0,0 +1,8 @@
+namespace MarketHub.Models
+{
+    public class MongoDBSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
