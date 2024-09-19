581663fcaee4aa958388d09c268668976838d7ba
harith2001 <danula243@gmail.com>
2024-09-19 07:25:49 +0530

    Revert "inital commit : web service (MongoDB connection Added)"
    This reverts commit fb6531f96721a4a4f68306495d7f1b941bf93826.

--- Models/MongoDBSettings.cs
+++
@@ -1,8 +0,0 @@
-namespace MarketHub.Models
-{
-    public class MongoDBSettings
-    {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-    }
-}
